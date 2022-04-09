d9c12bf79a2e0e5c997c526ad3f97365061c16e5
motrogue <83954210+motrogue@users.noreply.github.com>
2022-04-09 19:18:01 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,84 @@
+<?php
+$tmp=file_get_contents("lab7.tpl");  // Загрузка шаблона впеременную
+$objects="";  // Очистка переменных
+$content="";
+	$C= $_GET["C"];
+	   switch($C)  {
+	        case "1": $objects=file_get_contents("objects1.php")); break;
+	        case "2": $objects=file_get_contents("objects2.php")); break;
+	        case "3": $objects=file_get_contents("objects3.php")); break;
+	        default: $objects=file_get_contents("about.php");
+	   }
+	$O=$_GET["O"];
+	   switch($O) {		     
+		case "1-1": $content=file_get_contents("object1-1.php"); break;
+		case "1-2": $content=file_get_contents("object1-2.php"); break;
+		case "1-3": $content=file_get_contents("object1-3.php"); break;
+		case "2-1": $content=file_get_contents("object2-1.php"); break;
+		case "2-2": $content=file_get_contents("object2-2.php"); break;
+		case "2-3": $content=file_get_contents("object2-3.php"); break;
+		case "3-1": $content=file_get_contents("object3-1.php"); break;
+		case "3-2": $content=file_get_contents("object3-2.php"); break;
+		case "3-3": $content=file_get_contents("object3-3.php"); break;
+		default: $content=file_get_contents("applicate.php");
+	   };
+	
+	$tmp = str_replace("{object}",$objects, $tmp);
+	$tmp = str_replace("{content}",$contents, $tmp);
+echo $tmp;
+?>
+<?php
+$tmp=file_get_contents("lab7.tpl");  // Подключение файла с шаблоном
+$objects=""; 
+$content="";
+			
+function load_content($s) // Эта функция будет вызываться вместо вывода данных браузеру
+{
+    $GLOBALS['description'].=$s; // Новые данные добавляем к переменной
+}
+$C= $_GET["C"];
+	   switch($C)  {
+	        case "1": $objects=file_get_contents("objects1.php")); break;
+	        case "2": $objects=file_get_contents("objects2.php")); break;
+	        case "3": $objects=file_get_contents("objects3.php")); break;
+	        default: $objects=file_get_contents("about.php");
+	   }
+          $O= $_GET["O"];
+	   switch($O) {		     
+		case "1-1": $content=file_get_contents("object1-1.php"); break;
+		case "1-2": $content=file_get_contents("object1-2.php"); break;
+		case "1-3": $content=file_get_contents("object1-3.php"); break;
+		case "2-1": $content=file_get_contents("object2-1.php"); break;
+		case "2-2": $content=file_get_contents("object2-2.php"); break;
+		case "2-3": $content=file_get_contents("object2-3.php"); break;
+		case "3-1": $content=file_get_contents("object3-1.php"); break;
+		case "3-2": $content=file_get_contents("object3-2.php"); break;
+		case "3-3": $content=file_get_contents("object3-3.php"); break;
+	    case "99":{
+		       ob_start('load_content'); // Включаем кеширование и указываем, что вместо отправки данных клиенту, их нужно отправлять в функцию
+		       include("registration_form.php");
+		       ob_end_flush();   // Выключаем кеширование... теперь все данные будут уходить браузеру
+		       break;
+	    }
+		default: $content=file_get_contents("applicate.php");	
+	    };
+$tmp = str_replace("{object}",$objects, $tmp);
+$tmp = str_replace("{content}",$contents, $tmp);
+echo $tmp;
+?>
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
---
+++ index.tpl
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+		<head>
+		 <title>Лабораторная работа (Поклад и Акишев production)</title>
+		 <link rel="stylesheet" type="text/css" href="style.css" />
+		</head>
+		<body>
+			<div id="categories">
+			  Категории
+			  
+			  <ul id="list">
+				<a href="lab6.php?C=1"> Категория 1</a>
+				<a href="lab6.php?C=2"> Категория 2</a>
+				<a href="lab6.php?C=3"> Категория 3</a>
+				<a href="lab6.php"> Домой</a>
+			 </div>
+				<div id="conteiner">
+					<div id="objects">
+			{objects}
+					</div>
+					<div id="content">
+			{content}
+		        		</div>
+				</div>
+		</body>
